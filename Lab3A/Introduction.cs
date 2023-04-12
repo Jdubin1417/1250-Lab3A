1b7075400df7b8bb5827ffb43f3568c8a9b69f51
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2023-04-12 08:40:52 -0400

    Add files via upload

---
+++ Lab3A/Introduction.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstName;
+string lastName;
+string ageAsString;
+int age;
+Console.Write("What is your first name?");
+firstName = Console.ReadLine();
+Console.Write("What is your last name?");
+lastName = Console.ReadLine();
+Console.WriteLine("How old are you?");
+ageAsString = Console.ReadLine();
+age = Convert.ToInt32(ageAsString);
+Console.Write("Hello, " + firstName + " " + 
+                          lastName + "! Your initials are " + 
+                          firstName[0] + "." + 
+                          lastName[0] + "., and you are " + 
+                          age + " years old.");
